5f8d1a28a2e6ff3426c331ac74efe03960f02b77
Anna <anna_agabekyan_anna@mail.ru>
2020-07-08 11:53:04 +0400

    add auth

---
+++ script.js
@@ -0,0 +1,9 @@
+// $(document).ready(function(){
+//     $('.form-container').on('click',".tab",function(){
+//         $('.form-container').find('.active').removeClass('active')
+//         $(this).addClass("active")
+
+//         $(this).addClass("active")
+//         $(".tab-form").eq($(this).index()).addClass('active')
+//     })
+// })
